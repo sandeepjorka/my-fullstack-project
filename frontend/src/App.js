dc53fcf32e53f77d100df1858291a7006bf6cfac
sandeep <you@example.com>
2025-07-08 20:46:51 +0530

    changed to jorka sandeep

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import AddUser from './AddUser';
 function App() {
   return (
     <div className="App">
-      <h1>🚀 Powered Retail AI</h1>
+      <h1>🚀 Jorka sandeep kumar</h1>
       <AddUser />
       <hr />
       <Users />
