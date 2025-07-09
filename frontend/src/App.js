969b53e78871162bfea1d477dd07a5f641b1fe2f
sandeep <you@example.com>
2025-07-09 10:27:48 +0530

    changed to mr raj

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import AddUser from './AddUser';
 function App() {
   return (
     <div className="App">
-      <h1>🚀 TARA RETAIL AII</h1>
+      <h1>🚀 MR RAJ </h1>
       <AddUser />
       <hr />
       <Users />
